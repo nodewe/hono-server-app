30271e603f2e26581389720a7f6fd9d86678b86b
Evan_huang <3160243061@qq.com>
2025-09-01 17:39:12 +0800

    feat:添加错误日志

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import fs from 'fs/promises'
+import dayjs from "dayjs"
+dayjs.locale('zh-cn')
 import { HTTPException } from 'hono/http-exception'
 import { serveStatic } from '@hono/node-server/serve-static'
-
-import { logger } from 'hono/logger'
+import {ensureDirForFile} from "./utils/index.ts"
 import os from "os"
 import adminRouter from "./routes/admin/index.ts"
+
+import dotenv from 'dotenv'
+const nodeEnv = process.env.NODE_ENV || 'development'
+dotenv.config({ path: `.env.${nodeEnv}` })
+
 const app = new Hono()
+
+// app.use(ctxMiddleware)
 
 //静态托管
 app.use('/static/*', serveStatic({ rewriteRequestPath(path, c) {
@@ -15,7 +23,7 @@ app.use('/static/*', serveStatic({ rewriteRequestPath(path, c) {
   const path1 =  path.replace(/^\/static/, 'uploads')
   // console.log(path1,'path')
   return path1
-}, }))
+}, }));
 
 
 app.get('/', (c) => {
@@ -27,9 +35,28 @@ app.get('/', (c) => {
 app.route('/admin',adminRouter)
 
 // 监听全局错误
-app.onError((err, c) => {
-  console.error(err)
-  return c.text('Error', 500)
+app.onError(async (err,ctx)=>{
+  // console.log(err,'')
+  const method = ctx.req.method
+  const url = ctx.req.url
+  const curTime = dayjs()
+  const LOG_PATH = `logs/${curTime.format('YYYY-MM-DD')}-error.log`;
+  await ensureDirForFile(LOG_PATH)
+
+  fs.appendFile(LOG_PATH,`[${curTime.format('HH:mm:ss')}] ${method} ${url} ERROR - \n ${err.stack} \n`)
+
+  console.log(process.env.NODE_ENV,'env')
+
+  
+  // if(process.env.NODE_ENV === 'PROD'){
+  //   return ctx.text('internal server error')
+  // }
+
+  // console.log(err.stack,'stack')
+  return ctx.json({
+    code:500,
+    msg:err.message
+  });
 })
 
 serve({
