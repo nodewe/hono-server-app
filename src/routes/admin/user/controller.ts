dba03ffb0a424f9d357f2795215976da68f9a7c9
Evan_huang <3160243061@qq.com>
2025-09-14 13:30:15 +0800

    feat:用户的增删改查业务提交

--- src/routes/admin/user/controller.ts
+++ src/routes/admin/user/controller.ts
@@ -11,8 +11,41 @@ user.post("/add", async (ctx) => {
     // console.log(ctx.req.jso)
     const body = await ctx.req.json()
     // console.log(body, '添加')
-    await service.add()
-    return ctx.success({ msg: "添加用户成功" })
+    if (!body.username) {
+        return ctx.fail({ msg: "用户名不能为空" })
+    }
+    if (!body.nickname) {
+        return ctx.fail({ msg: "昵称不能为空" })
+    }
+    if (Array.isArray(body.roleIds) && !body.roleIds.length) {
+        return ctx.fail({ msg: "请选择角色" })
+    }
+    if (!body.deptId) {
+        // return ctx.fail({ msg: "请选择部门" })
+    }
+    if (!body.mobile) {
+        return ctx.fail({ msg: "手机号不能为空" })
+    }
+    //随机生成密码
+    body.password = Math.random().toString(36).substr(2, 10);
+    // 删除
+    body.deleted = 0;
+
+    const user = {
+        username: body.username || "",
+        nickname: body.nickname || "",
+        avatar: body.avatar || "",
+        deptId: body.deptId || "",
+        mobile: body.mobile || "",
+        email: body.email || "",
+        password: body.password
+    };
+    const isOk = await service.add(user, body.roleIds)
+    if (isOk) {
+        return ctx.success({ msg: "添加用户成功" })
+
+    }
+    return ctx.fail({ msg: "添加失败" })
 });
 
 /**
@@ -20,52 +53,101 @@ user.post("/add", async (ctx) => {
 */
 user.post("/login", async (ctx) => {
     const body = await ctx.req.json();
-    console.log(body, '登录');
+    // console.log(body, '登录');
     if (!body.username || !body.password) {
         return ctx.fail({ msg: "参数错误" })
     }
     const captcha = ctx.session.captcha
-    if(body.captcha !== captcha){
+    if (body.captcha !== captcha) {
         return ctx.fail({ msg: "验证码错误" })
     }
     // console.log(body, '登录')
     const info = await service.login(body.username, body.password);
 
-    if(!info){
+    if (!info) {
         return ctx.fail({ msg: "用户不存在" })
     }
     //生成token
     //@ts-ignore
-    const token = jwt.sign({id:info.id},jwtConfig.secret,{expiresIn:jwtConfig.expiresIn})
+    const token = jwt.sign({ id: info.id }, jwtConfig.secret, { expiresIn: jwtConfig.expiresIn })
 
     const data = {
         token
     }
-    return ctx.success({data})
+    return ctx.success({ data })
 });
 
 //获取当前用户信息
 user.get("/me", async (ctx) => {
     // const body = await ctx.req.json()
     // console.log(ctx.state.userInfo, '获取当前用户信息')
-    return ctx.success({data: ctx.state.userInfo})
+    return ctx.success({ data: ctx.state.userInfo })
 });
+
+/**
+* @description 修改用户状态
+*/
+user.put("/status",async (ctx) => {
+    const body = await ctx.req.json();
+    // console.log(body, '修改用户状态')
+    if (!body.userId) {
+        return ctx.fail({ msg: "参数错误" })
+    }
+
+    const statusOptions = [0,1]
+    if(!statusOptions.includes(body.status)){
+        return ctx.fail({ msg: "参数错误" })
+    }
+    const isOk = await service.changeStatus(body.userId, body.status)
+    if (!isOk) {
+        return ctx.fail({ msg: "修改失败" })
+    }
+    return ctx.success({ msg: "修改用户状态成功" })
+});
+
+/**
+* @description 重置密码
+*/
+user.put("/resetPassword", async (ctx) => { 
+    const body = await ctx.req.json();
+    // console.log(body, '重置密码')
+    if (!body.userId) {
+        return ctx.fail({ msg: "参数错误" })
+    }
+    if (!body.password) {
+        return ctx.fail({ msg: "参数错误" })
+    }
+    const isOk = await service.resetPassword(body.userId,body.password)
+    if (!isOk) {
+        return ctx.fail({ msg: "重置密码失败" })
+    }
+    return ctx.success({ msg: "重置密码成功" })
+})
 
 
 //获取个人用户列表
 user.get('/page', async (ctx) => {
-    const query = await ctx.req.query()
+    const query =  ctx.req.query()
     // console.log(body, '用户列表')
     let page = 1,
-        size = 10;
+        size = 10,
+        keywords = '',
+        status = '';
     if (query.pageNum) {
         page = +query.pageNum || page
     }
     if (query.pageSize) {
         size = +query.pageSize || size
     }
+    // 关键字
+    if(query.keywords){
+        keywords = query.keywords
+    }
+    if(query.status){
+        status = query.status
+    }
 
-    const userList = await service.page(page, size)
+    const userList = await service.page(page, size,keywords,status)
     // console.log(userList,'userList')
     return ctx.success({ data: userList, msg: "成功" })
     // return ctx.text("获取个人用户列表")
@@ -75,10 +157,10 @@ user.get('/page', async (ctx) => {
 //获取个人信息 根据id
 user.get("/info", async (ctx) => {
     const params = await ctx.req.query()
-    if (!params.id) {
+    if (!params.userId) {
         return ctx.fail({ msg: "参数错误" })
     }
-    const info = await service.info(+params.id);
+    const info = await service.info(+params.userId);
     if (!info) {
         return ctx.fail({ msg: "用户不存在" })
     }
@@ -88,16 +170,40 @@ user.get("/info", async (ctx) => {
 // 修改用户
 user.put("/update", async (ctx) => {
     const body = await ctx.req.json()
-    console.log(body, '修改')
-    return ctx.text("修改用户")
+    if (!body.id) {
+        return ctx.fail({ msg: "用户参数错误" })
+    }
+    if (!body.username) {
+        return ctx.fail({ msg: "用户名不能为空" })
+    }
+    if (!body.nickname) {
+        return ctx.fail({ msg: "昵称不能为空" })
+    }
+    if (Array.isArray(body.roleIds) && !body.roleIds.length) {
+        return ctx.fail({ msg: "请选择角色" })
+    }
+    if (!body.deptId) {
+        // return ctx.fail({ msg: "请选择部门" })
+    }
+    if (!body.mobile) {
+        return ctx.fail({ msg: "手机号不能为空" })
+    }
+    delete body.password
+    const isOk = await service.update(body, body.roleIds);
+    if (!isOk) {
+        return ctx.fail({ msg: "修改失败" })
+    }
+    return ctx.success({ msg: "修改用户成功" })
 });
 
 // 删除用户
 user.delete("/delete", async (ctx) => {
     const body = await ctx.req.json()
-    console.log(body, '删除')
     if (Array.isArray(body) && body.length) {
-
+        const ret = await service.del(body)
+        if (!ret) {
+            return ctx.fail({ msg: "删除失败" })
+        }
         return ctx.json({
             code: 200,
             data: "删除用户成功"
--- src/routes/admin/user/service.ts
+++ src/routes/admin/user/service.ts
@@ -1,32 +1,123 @@
 import db from "@/mysql/index.ts"
 import { ISysUser } from "./userType.ts"
+import dayjs from "dayjs"
+import { toLikeStr } from "@/utils/index.ts"
 
 /**
 * @description 添加
 */
-const add = async () => {
+const add = async (user: ISysUser, roleId: string[]) => {
     const conn = await db.getConnection()
-
-    conn.beginTransaction()
+    await conn.beginTransaction()
     try {
-        const [result] = await conn.query('insert into user(username,password) values(?,?)', ['admin', '123456'])
+        const insertSQL = `
+            INSERT INTO sys_user(username,nickname,gender,password,deptId,avatar,mobile,email) VALUES(?,?,?,?,?,?,?,?)
+        `;
+        const params = [user.username, user.nickname, user.gender, user.password, user.deptId, user.avatar, user.mobile, user.email]
+        const [userAdd] = await conn.query(insertSQL, params);
+        const userAddRet = userAdd as queryResultType
+        // 如果添加成功 
+        if (userAddRet.affectedRows > 0) {
+            const insertList = roleId.map(ele => [ele, userAddRet.insertId]);
+            // 角色添加
+            const [roleAdd] = await conn.query(
+                'INSERT INTO sys_user_role (roleId,userId) values ?',
+                [insertList]
+            );
+            const roleAddRet = roleAdd as queryResultType
+            if (roleAddRet.affectedRows == insertList.length) {
+                await conn.commit()
+                conn.release()
+                return true
+            }
+        }
+        await conn.rollback()
+        conn.release()
+        return false
     } catch (error) {
-
+        await conn.rollback()
+        conn.release()
+        throw error
     }
+
 }
 
 /**
-* @description 更新
+* @description 更新用户
 */
-const update = () => {
-
+const update = async (user: ISysUser, roleIds: string[]) => {
+    const conn = await db.getConnection();
+    await conn.beginTransaction();
+    const sql = `
+        update sys_user set username = ?,nickname = ?,gender = ?,deptId = ?,avatar = ?,mobile = ?,email = ? where id = ?
+    `;
+    try {
+        //更新用户基本信息
+        const [result] = await conn.query(sql, [user.username, user.nickname, user.gender, user.deptId, user.avatar, user.mobile, user.email, user.id]);
+        const resultRet = result as queryResultType;
+        if (resultRet.affectedRows > 0) {
+            //更新用户对应的角色信息 
+            const [roleUpdate] = await conn.query(
+                'delete from sys_user_role where userId = ?',
+                [user.id]
+            );
+            const roleUpdateRet = roleUpdate as queryResultType
+            if (roleUpdateRet.affectedRows > 0) {
+                const insertList = roleIds.map(ele => [ele, user.id]);
+                // 角色添加
+                const [roleAdd] = await conn.query(
+                    'INSERT INTO sys_user_role (roleId,userId) values ?',
+                    [insertList]
+                );
+                const roleAddRet = roleAdd as queryResultType
+                if (roleAddRet.affectedRows == insertList.length) {
+                    await conn.commit()
+                    conn.release()
+                    return true
+                }
+            }
+        }
+        await conn.rollback()
+        conn.release()
+        return false
+    } catch (error) {
+        await conn.rollback()
+        conn.release()
+        throw error
+    }
 }
 
 /**
 * @description 删除
 */
-const del = () => {
+const del = async (ids: string[]) => {
+    const conn = await db.getConnection();
+    await conn.beginTransaction();
+    try {
 
+        const [result] = await conn.query('delete from sys_user where id in (?)', [ids]);
+        const resultRet = result as queryResultType
+        if (resultRet.affectedRows > 0) {
+            // 删除对应的角色
+            const [roleDel] = await conn.query(
+                'delete from sys_user_role where userId in (?)',
+                [ids]
+            );
+            const roleDelRet = roleDel as queryResultType
+            if (roleDelRet.affectedRows > 0) {
+                await conn.commit()
+                conn.release()
+                return true
+            }
+        }
+        await conn.rollback()
+        conn.release()
+        return false
+    } catch (error) {
+        await conn.rollback()
+        conn.release()
+        throw error
+    }
 }
 
 /**
@@ -35,10 +126,22 @@ const del = () => {
 const info = async (userId: number) => {
 
     const conn = await db.getConnection();
-    const [result] = await conn.query('select id,username,nickname,gender,dept_id,avatar,mobile,status,email,create_time,update_time from sys_user where id = ?', [userId]);
+    const [result] = await conn.query('select id,username,nickname,gender,deptId,avatar,mobile,status,email from sys_user where id = ?', [userId]);
+    // 查询对应的角色
+    const [roleList] = await conn.query(
+        'SELECT roleId FROM sys_user_role where userId = ?',
+        [userId]
+    );
+
     await conn.release();
+    const roleIds = roleList as Array<{ roleId: number }>;
     const rets = result as Array<ISysUser>;
     const userInfo = rets[0];
+    if (!userInfo) {
+        return false;
+    }
+    //@ts-ignore
+    userInfo.roleIds = roleIds.map(ele => ele.roleId);
     return userInfo;
 
 
@@ -47,16 +150,26 @@ const info = async (userId: number) => {
 /**
 * @description 分页获取用户列表
 */
-const page = async (page: number, pageSize: number) => {
+const page = async (page: number, pageSize: number, keywords: string, status: string) => {
 
     const conn = await db.getConnection();
-  
-    const [result] = await conn.query('select id,username,nickname,gender,dept_id,avatar,mobile,status,email,create_time,update_time from sys_user limit ?,?', [(page - 1) * pageSize, pageSize]);
+    const sql = `
+    select id,username,nickname,gender,deptId,avatar,mobile,status,email,createTime from sys_user where concat(nickname,username,mobile) LIKE CONCAT('%', ?, '%') 
+        and IF(''=?,TRUE,CAST(status AS SIGNED)=?) limit ?,?`;
+    const [result] = await conn.query(sql, [
+        toLikeStr(keywords),
+        status, status,
+        (page - 1) * pageSize, pageSize]);
     const rets = result as Array<ISysUser>;
+    // 循环处理时间
+    rets.forEach(item => {
+        item.createTime = item.createTime ? dayjs(item.createTime).format('YYYY-MM-DD HH:mm:ss') : "-";
+        item.updateTime = item.updateTime ? dayjs(item.updateTime).format('YYYY-MM-DD HH:mm:ss') : "-";
+    });
     //查出总数
-    const [total] = await conn.query('select count(id) as total from sys_user');
+    const [total] = await conn.query(`select count(id) as total,status from sys_user where concat(nickname,username,mobile) LIKE CONCAT('%', ?, '%') and IF(''=?,TRUE,CAST(status AS SIGNED)=?)`, [toLikeStr(keywords), status, status]);
     const totalCount = (total as Array<{ total: number }>)[0].total;
-    console.log('totalCount', totalCount);
+    // console.log('totalCount', totalCount);
     await conn.release();
     return {
         total: totalCount,
@@ -72,14 +185,47 @@ const page = async (page: number, pageSize: number) => {
 */
 const login = async (username: string, password: string) => {
     const conn = await db.getConnection();
-    const [result] = await conn.query('select id,username,nickname,gender,dept_id,avatar,mobile,status,email,create_time,update_time from sys_user where username = ? and password = ?', [username, password]);
+    const [result] = await conn.query('select id,username,nickname,gender,deptId,avatar,mobile,status,email from sys_user where username = ? and password = ?', [username, password]);
+    // 释放连接
     await conn.release();
     const rets = result as Array<ISysUser>;
-    // await conn.commit();
     return rets[0];
 }
 
+/**
+* @description 修改用户状态
+
+*/
+const changeStatus = async (userId: number, status: number) => { 
+    const conn = await db.getConnection();
+    const [result] = await conn.query('update sys_user set status = ? where id = ?', [status, userId]);
+    const resultRet = result as queryResultType
+    if (resultRet.affectedRows > 0) {
+        await conn.release()
+        return true
+    }
+    await conn.release()
+    return false
+}
+/**
+* @description 重置密码
+*/
+const resetPassword = async (userId: number, password: string) => { 
+    const conn = await db.getConnection();
+    const [result] = await conn.query('update sys_user set password = ? where id = ?', [password, userId]);
+    const resultRet = result as queryResultType
+    if (resultRet.affectedRows > 0) {
+        await conn.release()
+        return true
+    }
+    await conn.release()
+    return false
+}
+
+
 export default {
+    resetPassword,
+    changeStatus,
     add,
     update,
     del,
