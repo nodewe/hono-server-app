7cad4960e3edca245887e38033b9a792fdf24eca
Evan_huang <3160243061@qq.com>
2025-09-14 15:44:55 +0800

    feat：菜单增删改查的业务逻辑添加

--- src/routes/admin/menu/controller.ts
+++ src/routes/admin/menu/controller.ts
@@ -2,13 +2,70 @@
 import { Hono } from "hono";
 import service from "./service.ts";
 import { buildTree } from "@/utils/index.ts";
+import dayjs from "dayjs";
 
 const menu = new Hono();
 
 /**
+* @description 添加菜单
+*/
+menu.post('/add', async (ctx) => {
+  const body = await ctx.req.json()
+  // console.log(body, '添加菜单')
+  const types = {
+    'CATALOG':1,
+    'MENU':2,
+    'EXTLINK':3,
+    'BUTTON':4,
+  };
+  //@ts-ignore
+  body.type = types[body.type]
+  const res = await service.add(body)
+  if(res){
+    return ctx.success({  msg: "添加成功" })
+
+  }
+  return ctx.fail({ msg: "添加成功" })
+});
+
+
+/**
+* @description 修改菜单
+*/
+menu.put('/update', async (ctx) => {
+  const body = await ctx.req.json()
+  // console.log(body, '修改菜单')
+  const types = {
+    'CATALOG':1,
+    'MENU':2,
+    'EXTLINK':3,
+    'BUTTON':4,
+  };
+  //@ts-ignore
+  body.type = types[body.type]
+  body.updateTime = dayjs().toISOString()
+  const res = await service.update(body)
+  if(res){
+    return ctx.success({  msg: "修改成功" })
+
+  }
+  return ctx.fail({ msg: "修改成功" })
+});
+
+menu.delete('/delete',async (ctx) => { 
+  const body = await ctx.req.json()
+      // console.log(body, '删除')
+      if (Array.isArray(body) && body.length) {
+          await service.del(body)
+        
+          return ctx.success({ msg: '删除成功' })
+      }
+});
+
+/**
 * @description 获取菜单选项列表
 */
-menu.get('/options',async (ctx)=>{
+menu.get('/options', async (ctx) => {
   const menuList = await service.options()
   if (menuList.length) {
     const data = buildTree({
@@ -28,69 +85,111 @@ menu.get('/options',async (ctx)=>{
         };
       },
     });
-    return ctx.success({data})
+    return ctx.success({ data })
   }
-  return ctx.fail({data:[]})
- 
+  return ctx.fail({ data: [] })
+
 });
 /**
 * @description 获取菜单列表
 */
 
-menu.get('/route/list',async (ctx)=>{
-    try {
-        const roleIds = ctx.state.userInfo.roleIds;
-        //角色的code 集合
-        const roles = ctx.state.userInfo.roles;
-        // 根节点
-        let menuList = await service.list(roleIds.join(","))
-        //当前角色能访问的 菜单筛选出来
-        menuList = buildTree({
-          list: menuList,
-            //@ts-ignore
-          parentFunc(item) {
-            return {
-              sort: item.sort,
-              component: item.component ? item.component : undefined,
-              path: item.path ? item.path : undefined,
-              redirect: item.redirect ? item.redirect : undefined,
-              meta: {
-                title: item.name,
-                hidden: item.visible != 1,
-                icon: item.icon,
-                keepAlive: true,
-                roles,
-              },
-            };
+menu.get('/tree/list', async (ctx) => {
+  let keywords = "";
+  const query = ctx.req.query();
+  if (query.keywords) {
+    keywords = query.keywords;
+  }
+  //                          目录  菜单      外链   按钮
+  const types = ['', 'CATALOG', 'MENU', 'EXTLINK', 'BUTTON'];
+  const menuList = await service.treeList(keywords);
+
+  const data = buildTree({
+    list: menuList,
+    //@ts-ignore
+    parentFunc(item) {
+      item.type = types[item.type];
+      return item;
+    },
+    //@ts-ignore
+    childFunc(item) {
+      item.type = types[item.type];
+      return item;
+    },
+  });
+  return ctx.success({ data })
+
+});
+
+menu.get('/info',async (ctx) => { 
+  const query = ctx.req.query()
+  if(!query.menuId){
+    return ctx.fail({ msg: '请输入菜单id' })
+  }
+  const menu = await service.info(+query.menuId)
+  if(!menu){
+    return ctx.fail({ msg: '菜单不存在' })
+  }
+  const types = [ '', 'CATALOG', 'MENU', 'EXTLINK', 'BUTTON' ];
+  //@ts-ignore
+  menu.type = types[menu.type];
+  return ctx.success({ data: menu, msg: "成功" })
+})
+
+menu.get('/route/list', async (ctx) => {
+  try {
+    const roleIds = ctx.state.userInfo.roleIds;
+    //角色的code 集合
+    const roles = ctx.state.userInfo.roles;
+    // 根节点
+    let menuList = await service.list(roleIds.join(","))
+    //当前角色能访问的 菜单筛选出来
+    menuList = buildTree({
+      list: menuList,
+      //@ts-ignore
+      parentFunc(item) {
+        return {
+          sort: item.sort,
+          component: item.component ? item.component : undefined,
+          path: item.path ? item.path : undefined,
+          redirect: item.redirect ? item.redirect : undefined,
+          meta: {
+            title: item.name,
+            hidden: item.visible != 1,
+            icon: item.icon,
+            keepAlive: true,
+            roles,
           },
-          //@ts-ignore
-          childFunc(item) {
-            return {
-              sort: item.sort,
-              component: item.component ? item.component : undefined,
-              path: item.path ? item.path : undefined,
-              // redirect:item.redirect,
-              name: item.path ? item.path.slice(0, 1)
-                .toUpperCase() + item.path.slice(1)
-                .toLowerCase() : undefined,
-              meta: {
-                title: item.name,
-                hidden: item.visible != 1,
-                icon: item.icon,
-                keepAlive: true,
-                roles,
-              },
-            };
+        };
+      },
+      //@ts-ignore
+      childFunc(item) {
+        return {
+          sort: item.sort,
+          component: item.component ? item.component : undefined,
+          path: item.path ? item.path : undefined,
+          // redirect:item.redirect,
+          name: item.path ? item.path.slice(0, 1)
+            .toUpperCase() + item.path.slice(1)
+              .toLowerCase() : undefined,
+          meta: {
+            title: item.name,
+            hidden: item.visible != 1,
+            icon: item.icon,
+            keepAlive: true,
+            roles,
           },
-        });
-        
-  
-        // await conn.commit()
-        return ctx.success({data: menuList})
-      } catch (e) {
-        //    await conn.rollback()
-        console.log(e);
-      }
+        };
+      },
+    });
+
+
+    // await conn.commit()
+    return ctx.success({ data: menuList })
+  } catch (e) {
+    //    await conn.rollback()
+    console.log(e);
+  }
 });
 
 
--- src/routes/admin/menu/service.ts
+++ src/routes/admin/menu/service.ts
@@ -1,8 +1,9 @@
 import db from "@/mysql/index.ts"
 import { ISysMenu } from "./menuType.ts";
+import { toLikeStr } from "@/utils/index.ts";
 
 /**
-* @description 获取菜单列表
+* @description 获取菜单列表展示
 */
 const list = async (roleId:string) => {
      const conn = await db.getConnection()
@@ -27,7 +28,137 @@ const options = async () => {
 
 }
 
+/**
+* @description 获取菜单管理中的树形列表管理
+*/
+const treeList = async (keywords:string) => { 
+   
+      const conn = await db.getConnection()
+      const sql = `select * from sys_menu where name LIKE concat('%',?,'%')`;
+
+      const [result] = await conn.query(sql, [
+        toLikeStr(keywords)
+      ]);
+      const menuListRet = result as ISysMenu[]
+      await conn.release()
+      return menuListRet 
+    
+}
+
+
+/**
+* @description 添加菜单
+*/
+const add = async (menu: ISysMenu) => { 
+    const conn = await db.getConnection()
+    await conn.beginTransaction()
+    try {
+        if(menu.parentId == '0'){
+            menu.treePath = '0'
+        }else{
+            
+            const sql = `select treePath from sys_menu where id = ?`
+            const [result] = await conn.query(sql, [menu.parentId])
+            
+            const resultRet = result as Array<{ treePath: string }>
+            menu.treePath = resultRet[0].treePath + ',' + menu.parentId
+        }
+       
+        // 插入菜单
+        const sql = `insert into sys_menu set ?`
+        const [menuResult] = await conn.query(sql, [menu])
+        const menuRes = menuResult as queryResultType
+        await conn.commit()
+        await conn.release()
+        if(menuRes.affectedRows > 0){
+            return true
+        }
+        return false
+    } catch (error) {
+        await conn.rollback()
+        conn.release()
+        throw error
+    }
+    
+}
+
+/**
+* @description 获取菜单详情
+*/
+const info = async (menuId: number) => {    
+    const conn = await db.getConnection()
+    const sql = `select * from sys_menu where id = ?`;
+    const [result] = await conn.query(sql, [menuId])
+    const menuInfoRet = result as ISysMenu[]
+    await conn.release()
+    return menuInfoRet[0]
+
+}
+/**
+* @description 删除
+*/
+const del = async (menuIds: string[]) => { 
+    const conn = await db.getConnection()
+    const sql = `delete from sys_menu where id in (?)`
+    await conn.query(sql, [menuIds])
+    await conn.release()
+}
+
+/**
+* @description 修改
+*/
+const update = async (menu: ISysMenu) => { 
+
+  const conn = await db.getConnection()
+    await conn.beginTransaction()
+    try {
+        if(menu.parentId == '0'){
+            menu.treePath = '0'
+        }else{
+            
+            const sql = `select treePath from sys_menu where id = ?`
+            const [result] = await conn.query(sql, [menu.parentId])
+            
+            const resultRet = result as Array<{ treePath: string }>
+            menu.treePath = resultRet[0].treePath + ',' + menu.parentId
+        }
+       
+        // 修改菜单
+        const sql = `update sys_menu set name = ?,type = ?,path = ?,component = ?,perm = ?,visible = ?,sort = ?,icon = ?,redirect = ?,treePath = ?,updateTime = ? where id = ?`
+        const [menuResult] = await conn.query(sql, [
+            menu.name,
+            menu.type,
+            menu.path,
+            menu.component,
+            menu.perm,
+            menu.visible,
+            menu.sort,
+            menu.icon,
+            menu.redirect,
+            menu.treePath,
+            menu.updateTime,
+            menu.id
+        ])
+        const menuRes = menuResult as queryResultType
+        await conn.commit()
+        await conn.release()
+        if(menuRes.affectedRows > 0){
+            return true
+        }
+        return false
+    } catch (error) {
+        await conn.rollback()
+        conn.release()
+        throw error
+    }
+    
+}
 export default{
     list,
-    options
+    options,
+    treeList,
+    add,
+    del,
+    update,
+    info
 }
