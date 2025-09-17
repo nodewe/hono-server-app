98158403631c84472a3cc10f5ece968b3b6c379c
Evan_huang <3160243061@qq.com>
2025-09-17 09:29:45 +0800

    feat:更改打包配置文件

--- build.js
+++ build.js
@@ -1,12 +1,18 @@
-// import { build } from 'esbuild';
-const { build } = require('esbuild')
+import { build } from 'esbuild';
+import fs from 'fs/promises';
+import path from 'path';
 
-build({
+await build({
     entryPoints: ['src/index.ts'],
     bundle: true,
-    outfile: 'dist/server.js',
+    outfile: 'dist/server.cjs',
     platform: 'node',
     format: 'cjs',
     target: 'node18',
-    external: ['config.js'],
+    external: ['config.json5'],
 })
+
+// 1. 复制 config.mjs 到 dist 目录
+const srcConfigPath = path.resolve('config.json5');
+const distConfigPath = path.resolve('dist/config.json5');
+await fs.copyFile(srcConfigPath, distConfigPath);
